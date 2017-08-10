ee4088529308a475cb9329704569928496e40735
Curtis Tarr <c.tarr@kainos.com>
2017-08-10 11:06:58 +0100

    sprint1 created

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,10 +2,6 @@ package app;
 
 public class Main {
 
-	public Main() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
